3612480a296a92c11dd48f25bd32e95407a7aeb9
customer <782385854@qq.com>
2016-07-04 14:38:27 +0800

    add pod file
    add pod file

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '7.0'
+pod 'pop', :git => 'https://github.com/facebook/pop.git'
--- popFirst/ViewController.m
+++ popFirst/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "POP.h"
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *customView;
 
 @end
 
@@ -17,11 +19,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGFloat tempw = 100;
+    CGFloat temph = 100;
+    CGFloat tempx = [UIScreen mainScreen].bounds.size.width / 2 - tempw / 2;
+    CGFloat tempy = [UIScreen mainScreen].bounds.size.height / 2 - temph / 2;
+
+    
+    
+    self.customView = [[UIView alloc]initWithFrame:CGRectMake(tempx, tempy, tempw, temph)];
+    self.customView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.customView];
+    
+  
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
+//    
+    NSInteger height = CGRectGetHeight(self.view.bounds);
+//    NSInteger width = CGRectGetWidth(self.view.bounds);
+//    
+//    CGFloat centerX = arc4random() % width;
+//    CGFloat centerY = arc4random() % height;
+    
+    CGFloat centerX = CGRectGetMaxX(self.customView.bounds);
+    CGFloat tempY = CGRectGetMaxY(self.customView.bounds);
+    
+    
+    CGFloat centerY = arc4random() % height;
+    
+    
+    
+    anim.toValue = [NSValue valueWithCGPoint:CGPointMake(centerX, centerY)];
+    anim.springBounciness = 16;
+    anim.springSpeed = 6;
+    [self.customView pop_addAnimation:anim forKey:@"center"];
+}
+
+
+
 
 @end
